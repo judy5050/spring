050d2a0c1d2e34b8733749996b040769971f8b59
judy5050 <68409061+judy5050@users.noreply.github.com>
2021-02-23 02:14:59 +0900

    자동, 수동의 올바른 싦무 운영 기준까지 완료

--- src/test/java/hello/core/scan/AutoAppConfigTest.java
+++ src/test/java/hello/core/scan/AutoAppConfigTest.java
@@ -14,6 +14,7 @@ public class AutoAppConfigTest {
 
     @Test
     void basicScan(){
+
         ApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class);
 
         MemberService memberService= ac.getBean(MemberService.class);
